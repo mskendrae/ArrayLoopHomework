5a6a2e309626f8d95d5090d7868691fa87167b7a
Kendra Egert <mskendrae@gmail.com>
2016-09-14 21:29:30 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLoopHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int winNumber = int.Parse(Console.ReadLine());
+
+            while (!(winNumber % 3 == 0))
+              { Console.WriteLine("you lost! please try again");
+            winNumber = int.Parse(Console.ReadLine());
+              }
+                Console.WriteLine("You WON!!!");
+
+        
+         }
+    }
+}
